7932f7be73a7040dbf601e8b483e4276b4f6835c
sudikonda <sudikonda.sivakumar@gmail.com>
2025-11-28 06:20:04 -0500

    feat: module federation - integration

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
     mode: 'development',
@@ -8,6 +9,12 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html'
+        }),
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                products: 'products@http://localhost:8081/remoteEntry.js'
+            }
         })
     ]
 }
